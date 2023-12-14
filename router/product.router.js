850f33eef7973c818becd6425c8a74272dbbc9ee
unknown <cherif.bellahouel161@gmail.com>
2023-12-14 12:56:50 +0100

    add db and products routes

--- router/product.router.js
+++ router/product.router.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
+const db = require('../database/db');
 
 router.get('/', async (req, res, next) => {
+    try {
+        const sql = "SELECT * FROM Products";
+        db.query(sql, (err, results) => {
+            if (err) {
+                console.log('Erreur lors de l\'affichage des produits');
+                res.status(500).json({ message : err })
+            } else {
+                res.status(200).json(results);
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:productId', async (req, res, next) => {
     try {
         // CODE ICI
     } catch (error) {
